cc564490437b0de8a3f4527ebca67269abbf014d
Jimmi Kardo Sitepu <32915206+jimmisitepu88@users.noreply.github.com>
2021-02-21 19:34:07 +0700

    Update test_rs485.ino

--- test_rs485/test_rs485.ino
+++ test_rs485/test_rs485.ino
@@ -1,6 +1,7 @@
+
 #include <ModbusMaster.h>
 ModbusMaster node;
-static uint8_t pzemSlaveAddr = 1;
+static uint8_t SlaveAddr = 1;
 
 #define RXD1 33
 #define TXD1 25
@@ -11,7 +12,7 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, RXD1, TXD1);
-  node.begin(pzemSlaveAddr, Serial1);
+  node.begin(SlaveAddr, Serial1);
 }
 
 void loop() {
@@ -26,11 +27,24 @@ void loop() {
 void baca_rs485(uint8_t wkt) {
   uint8_t result;
   uint32_t tempdouble;
-  result = node.readHoldingRegisters(171, 2);
+  result = node.readHoldingRegisters(0, 6);// address start ( list start from 1), get six register
   if (result == node.ku8MBSuccess)
   {
     Serial.println("succes1");
-    
+    tempdouble = node.getResponseBuffer(0);
+    Serial.println(tempdouble);
+    tempdouble = node.getResponseBuffer(1);
+    Serial.println(tempdouble);
+    tempdouble = node.getResponseBuffer(2);
+    Serial.println(tempdouble);
+    tempdouble = node.getResponseBuffer(3);
+    Serial.println(tempdouble);
+    tempdouble = node.getResponseBuffer(4);
+    Serial.println(tempdouble);
+    tempdouble = node.getResponseBuffer(5);
+    Serial.println(tempdouble);
+    tempdouble = node.getResponseBuffer(6);
+    Serial.println(tempdouble);
   }
   else {
     Serial.println("failed1");
